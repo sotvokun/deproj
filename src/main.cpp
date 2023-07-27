535b23fb382a5dc7a419fc33dec9b740415a5349
sotvokun <sotvokun@outlook.com>
2023-07-27 14:28:53 +0800

    init: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,93 @@
+#include "CLI11.hpp"
+#include "inja.hpp"
+#include "tmplt.h"
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::string readFileContent(const std::string &path);
+void writeFileContent(const std::string &path, const std::string &content);
+inja::json interactiveBuildContext(const std::string &content);
+
+struct AppOption {
+  std::string inputTemplate;
+  std::string outputPath;
+};
+
+int main(int argc, const char *argv[]) {
+  CLI::App app;
+  AppOption opt;
+
+  app.set_version_flag("-v,--version", tmplt::version());
+  app.add_option("TEMPLATE_NAME", opt.inputTemplate,
+                 "Template that need to render")
+      ->required();
+  app.add_option("-o,--output", opt.outputPath, "Output path")
+      ->default_val(std::filesystem::current_path())
+      ->default_str("");
+
+  CLI11_PARSE(app, argc, argv);
+
+  if (!std::filesystem::is_regular_file(opt.inputTemplate)) {
+    tmplt::die("Template file `" + opt.inputTemplate + "' not found");
+    return 1;
+  }
+
+  std::filesystem::path inputPath(opt.inputTemplate);
+  std::filesystem::path outputPath = opt.outputPath;
+  outputPath /= inputPath.stem();
+
+  auto templateContent = readFileContent(opt.inputTemplate);
+  auto result =
+      inja::render(templateContent, interactiveBuildContext(templateContent));
+
+  try {
+    writeFileContent(outputPath.string(), result);
+    std::cout << "Rendered template to " << outputPath.string() << std::endl;
+  } catch (std::exception &e) {
+    tmplt::die(e.what());
+  }
+
+  return 0;
+}
+
+std::string readFileContent(const std::string &path) {
+  if (!std::filesystem::is_regular_file(path) ||
+      !std::filesystem::exists(path)) {
+    tmplt::die("Template file `" + path + "' not found");
+  }
+
+  std::ifstream ifs(path);
+  std::string content((std::istreambuf_iterator<char>(ifs)),
+                      (std::istreambuf_iterator<char>()));
+  ifs.close();
+  return content;
+}
+
+void writeFileContent(const std::string &filePath , const std::string &content) {
+  std::ofstream ofs(filePath);
+  ofs << content;
+  ofs.close();
+}
+
+inja::json interactiveBuildContext(const std::string &content) {
+  inja::json data;
+  inja::Environment env;
+
+  tmplt::VariableVisitor visitor;
+  auto tmp = env.parse(content);
+  tmp.root.accept(visitor);
+
+  for (const auto &v : visitor.variables()) {
+    std::cout << "Enter value for "
+              << "\x1b[1m" << v << "\x1b[0m"
+              << ": ";
+    std::string value;
+    std::cin >> value;
+    data[v] = value;
+  }
+
+  return data;
+}
---
+++ src/tmplt.h
@@ -0,0 +1,78 @@
+#ifndef TMPLT_TMPLT_H
+#define TMPLT_TMPLT_H
+#include "inja.hpp"
+#include <string>
+#include <sstream>
+#include <vector>
+
+namespace tmplt {
+constexpr int VERSION_MAJOR = 0;
+constexpr int VERSION_MINOR = 1;
+constexpr int VERSION_PATCH = 0;
+
+inline void die(const std::string &msg) {
+  std::cerr << "\x1b[31m" << msg << "\x1b[0m" << std::endl;
+  exit(1);
+}
+
+inline std::string version() {
+  std::ostringstream oss;
+  oss << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH;
+  return oss.str();
+}
+
+class VariableVisitor : public inja::NodeVisitor {
+private:
+  std::vector<std::string> variables_;
+
+  void visit(const inja::BlockNode &node) {
+    for (const auto &n : node.nodes) {
+      n->accept(*this);
+    }
+  }
+  void visit(const inja::TextNode &) {}
+  void visit(const inja::ExpressionNode &) {}
+  void visit(const inja::LiteralNode &) {}
+  void visit(const inja::DataNode &n) { variables_.push_back(n.name); }
+
+  void visit(const inja::FunctionNode &node) {
+    for (const auto &n : node.arguments) {
+      n->accept(*this);
+    }
+  }
+
+  void visit(const inja::ExpressionListNode &node) { node.root->accept(*this); }
+
+  void visit(const inja::StatementNode &) {}
+  void visit(const inja::ForStatementNode &) {}
+
+  void visit(const inja::ForArrayStatementNode &node) {
+    node.condition.accept(*this);
+    node.body.accept(*this);
+  }
+
+  void visit(const inja::ForObjectStatementNode &node) {
+    node.condition.accept(*this);
+    node.body.accept(*this);
+  }
+
+  void visit(const inja::IfStatementNode &node) {
+    node.condition.accept(*this);
+    node.true_statement.accept(*this);
+    node.false_statement.accept(*this);
+  }
+
+  void visit(const inja::IncludeStatementNode &) {}
+
+  void visit(const inja::ExtendsStatementNode &) {}
+
+  void visit(const inja::BlockStatementNode &node) { node.block.accept(*this); }
+
+  void visit(const inja::SetStatementNode &) {}
+
+public:
+  const std::vector<std::string> &variables() const { return variables_; }
+};
+} // namespace tmplt
+
+#endif
